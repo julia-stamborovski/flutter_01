73ba0995fd1ef95f349dab7a01d08e82174d56f4
Julia Stamborovski <juliashizuko66@gmail.com>
2023-05-16 14:00:24 -0300

    column e row

--- lib/main.dart
+++ lib/main.dart
@@ -10,25 +10,66 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Stack(
-        alignment: AlignmentDirectional.center,
-        children: [
-          Container(
-            color: Colors.black,
-            width: 100,
-            height: 100,
-          ),
-          Container(
-            color: Colors.deepOrange,
-            width: 50,
-            height: 50,
-          ),
-        ],
-      ),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.pink,
+                  width: 200,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.deepPurple,
+                  width: 100,
+                  height: 100,
+                ),
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.deepPurple,
+                  width: 200,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.pink,
+                  width: 100,
+                  height: 100,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  height: 30,
+                  width: 30,
+                ),
+                Container(
+                  color: Colors.cyan,
+                  height: 30,
+                  width: 30,
+                ),
+                Container(
+                  color: Colors.red,
+                  height: 30,
+                  width: 30,
+                ),
+              ],
+            )
+          ],
+        ));
   }
 }
